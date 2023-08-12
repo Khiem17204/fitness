addd951ac6cdeddbbdce9e50e1778c51336ae9d2
Khiem Le <87161814+Khiem17204@users.noreply.github.com>
2023-08-12 14:04:47 +0700

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fitness
+//
+//  Created by Khiem Tran Le on 8/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
